608b38559fd50866560093fd4fe2d76dee19a257
Piotr Kolebski <piotrkolebski@gmail.com>
2020-05-14 18:09:45 +0200

    let specify column separator in csv file to load

--- data/load_dataset.py
+++ data/load_dataset.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import pandas as pd
 
 
-def load_dataset(path: Path):
-    dataset = pd.read_csv(path)
+def load_dataset(path: Path, separator: str = ','):
+    dataset = pd.read_csv(path, sep=separator)
     y = dataset.Target
     x = dataset.drop(columns=['Target'])
     return x.to_numpy(), y.to_numpy()
