a621a19066533165b87300e3ca762a59e8a95d67
Piotr Kolebski <piotrkolebski@gmail.com>
2020-05-14 18:08:55 +0200

    add gaussian voting method

--- voting_methods.py
+++ voting_methods.py
@@ -19,5 +19,13 @@ def distance_weighted_voting(distances: np.ndarray, k: int, epsilon: float = 1e-
     return get_prediction(votes)
 
 
+def gaussian_weighted_voting(distances: np.ndarray, k: int) -> int:
+    distances = sorted(distances, key=lambda x: x[0])[:k]
+    votes = defaultdict(float)
+    for dist, cls in distances:
+        votes[cls] += 1 / np.sqrt(2 * np.pi) * np.exp(-(dist ** 2) / 2)
+    return get_prediction(votes)
+
+
 def get_prediction(votes: dict) -> int:
     return max(votes.keys(), key=lambda key: votes[key])
